43ab1279b09a486d74412704ce55949a825e469a
arkasha401 <m1ramisama@gmail.com>
2022-03-13 01:17:19 +0200

    let's gooo

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ mod cpu;
 use cpu::CPU;
 use cpu::Instructions;
 fn main() {
+
     let mut cpu = CPU::new();
     let mut rom = Memory::new(4096);
     let mut ram = Memory::new(4096);
     rom.reset();
-    rom.set(0, 0xA0);
+    rom.set(4095, 0xA0);
+    rom.set(4096, 0xA1);
     cpu.reset();
-    let run_instr = Instructions::new();
+    let run_instr = Instructions::run_instructions();
+    
 }
