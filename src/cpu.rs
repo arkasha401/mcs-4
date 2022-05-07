37d6dc717bb670afa8e60a353554eac1f4f746f9
arkasha401 <m1ramisama@gmail.com>
2022-05-07 22:52:10 +0300

    a

--- src/cpu.rs
+++ src/cpu.rs
@@ -73,6 +73,7 @@ impl CPU {
             match upper_byte { 
                 0 => (),
                 10 => self.opr_ld(lower_byte),
+                11 => self.opr_xch(lower_byte),
                 13 => self.opr_ldm(lower_byte),
                 _ => ()
             }
@@ -103,6 +104,28 @@ impl CPU {
                 16 => self.a_r = self.r16,
                 _=> eprintln!("ERROR")
             }
+    }
+    pub fn opr_xch(&mut self, opa: u8) {
+        let temp: u8 = self.a_r;
+        match opa {
+            1 => {self.a_r = self.r1; self.r1 = temp},
+            2 => {self.a_r = self.r2; self.r2 = temp},
+            3 => {self.a_r = self.r3; self.r3 = temp},
+            4 => {self.a_r = self.r4; self.r4 = temp},
+            5 => {self.a_r = self.r5; self.r5 = temp},
+            6 => {self.a_r = self.r6; self.r6 = temp},
+            7 => {self.a_r = self.r7; self.r7 = temp},
+            8 => {self.a_r = self.r8; self.r8 = temp},
+            9 => {self.a_r = self.r9; self.r9 = temp},
+            10 => {self.a_r = self.r10; self.r10 = temp},
+            11 => {self.a_r = self.r11; self.r11 = temp},
+            12 => {self.a_r = self.r12; self.r12 = temp},
+            13 => {self.a_r = self.r13; self.r13 = temp},
+            14 => {self.a_r = self.r14; self.r14 = temp},
+            15 => {self.a_r = self.r15; self.r15 = temp},
+            16 => {self.a_r = self.r2; self.r2 = temp},
+            _=> eprintln!("ERROR")
+        } 
     }
 
 
