06590530c77d8b45a75f95a37957e20dc1b5d18b
arkasha401 <m1ramisama@gmail.com>
2022-07-14 00:26:58 +0300

    Memory struction has done

--- src/memory.rs
+++ src/memory.rs
@@ -7,23 +7,62 @@ const MAX_RAM: usize = 512;
 
 
 
-
+#[derive(Copy, Clone)]
 pub struct Register {
     main_memory: [u8; RAM_MAIN_MEMORY_CHARS],
     status_memory: [u8; RAM_STATUS_CHARS]
 }
 
 impl Register {
-
+    pub fn new() -> Register {
+        Register {
+            main_memory: [0; RAM_MAIN_MEMORY_CHARS],
+            status_memory: [0; RAM_STATUS_CHARS] 
+        }
+    }
 }
 
-
+ 
 pub struct RAM {
-    register: Register,
+    registers: [Register; NUMBER_OF_REGISTERS],
     output: u8
 }
 
+
 impl RAM {
+    pub fn new() -> RAM {
+        RAM {
+            registers: [
+                Register {
+                    main_memory: [0; RAM_MAIN_MEMORY_CHARS],
+                    status_memory: [0; RAM_STATUS_CHARS]
+                };
+            NUMBER_OF_REGISTERS],
+            output: 0 
+        }
+    }
+
+
+
+    pub fn read_main_char(&mut self,register: u8, character: u8,) -> u8 {
+        self.registers[register as usize].main_memory[character as usize]
+    }
+
+    pub fn write_main_char(&mut self,register: u8, character: u8, value: u8) {
+        self.registers[register as usize].main_memory[character as usize] = value
+    }
+
+    pub fn read_status_char(&mut self, register: u8, character: u8) -> u8 {
+        self.registers[register as usize].status_memory[character as usize]
+    }
+
+    pub fn write_status_char(&mut self, register: u8, character: u8, value: u8) {
+        self.registers[register as usize].status_memory[character as usize] = value  
+    }
+
+    pub fn ram_write_output(&mut self, value: u8) {
+        self.output = value
+    }
 
 }
 
@@ -31,6 +70,15 @@ pub struct ROM {
     data: [u8; ROM_SIZE],
     io: u8
 }
+
+
+
+impl Default for ROM {
+    fn default() -> Self {
+        Self::new(vec![0;255])
+    }
+}
+
 
 impl ROM {
     pub fn new(rom: Vec<u8>) -> ROM {
@@ -69,17 +117,19 @@ impl ROM {
 
 
 
-
-impl Default for ROM {
-    fn default() -> Self {
-        Self::new(vec![0;255])
-    }
-}
-
-
+#[derive()]
 pub struct Memory {
     pub ram: RAM,
-    pub rom: ()
+    pub rom: ROM
+}
+
+impl Memory {
+    pub fn new(instruction_list: Vec<u8>) -> Memory {
+        Memory {
+            ram: RAM::new(),
+            rom: ROM::new(instruction_list)
+        }
+    }
 }
 
 
