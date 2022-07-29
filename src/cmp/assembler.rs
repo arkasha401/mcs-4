8f437c3bacfc02a8a9e104dbdd30b3f94d857450
arkasha401 <m1ramisama@gmail.com>
2022-07-29 14:12:31 +0300

    Getting some troubles

--- src/cmp/assembler.rs
+++ src/cmp/assembler.rs
@@ -1,34 +1,37 @@
-use crate::cmp::dictionary;
+use crate::cmp::dictionary; 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::env;
 
 
+
 pub struct Assembler {
     data: Vec<String>, 
     binary: Vec<u8>, 
-} 
+}
 
 impl Assembler {
     pub fn new() -> Assembler {
         let name = env::args().nth(1).unwrap();
         let f = File::open(name).unwrap();
-        let mut reader = BufReader::new(f);
-        for (index, line) in reader.lines().enumerate() {
-            for i in line.unwrap().split_whitespace() {
-                println!("{}", i.trim())
-            }
-
-        } 
-
+        let mut temp_vec: Vec<String> = Vec::new();
+        let reader = BufReader::new(f);
+        for (_index,mut line) in reader.lines().enumerate() {
+            if line.unwrap().contains(";")  {
+                if line.unwrap().clone().as_bytes().last().unwrap() == ";".as_u8() {
+                }
+            } 
+            panic!("ERROR: SYNTAX ERROR MIGHT BE MISSED: ;")
+        }
         Assembler {
             data: Vec::new(),
             binary: Vec::new()
         }
     }
+}
+
 
     
-}
 
 
 
