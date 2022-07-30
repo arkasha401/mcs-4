716f9cc1f6201f18834b488d4638b3596a567607
arkasha401 <m1ramisama@gmail.com>
2022-07-30 15:16:53 +0300

    processing

--- src/cmp/assembler.rs
+++ src/cmp/assembler.rs
@@ -15,23 +15,43 @@ impl Assembler <'static> {
     pub fn new() -> Assembler <'static> {
         let name = env::args().nth(1).unwrap();
         let f = File::open(name).unwrap();
-        let mut temp_vec: Vec<String> = Vec::new();
         let reader = BufReader::new(f);
+        let mut temp_vec: Vec<String> = Vec::new();
         for (_index,line) in reader.lines().enumerate() {
-            let line = line.unwrap();
-            if line.contains(";")  {
-                if *line.as_bytes().last().unwrap() == (';' as u8) {
-                    temp_vec.push(line.replace(';', "").trim().to_string());
-                    println!("{:?}", temp_vec);
-                }    
-            } else if !line.contains(";"){
-                panic!("SYNTAX ERROR: ';' MISSED")
+            let _line = line.unwrap();
+            let line = _line.trim();
+            println!("{}", line);
+            if !line.is_empty(){
+                if line.trim().contains(";")  {
+                    if *line.as_bytes().last().unwrap() == (';' as u8) { 
+                        let temp_temp: Vec<_> = line.split_whitespace().collect(); 
+                        for element in temp_temp {
+                            if element.contains(';') {
+                                if (element.as_bytes())[0] == ';' as u8 {
+                                    ()
+                                } else if *element.as_bytes().last().unwrap() == (';' as u8){
+                                    temp_vec.push(element.replace(';', ""));
+                            }
+                            } else if !element.contains(';') {
+                                temp_vec.push(element.to_string())
+                            }
+                            
+                        println!("{:?}", temp_vec);
+                        }
+                    } else {
+                        panic!("ERROR: SYNTAX ERROR")
+                    }  
+                } else if !line.contains(";"){
+                    panic!("SYNTAX ERROR: ';' MISSED")
+                }
+            } else {
+                continue;
             }
-
-
         }
+
+        
         Assembler  {
-            data: Vec::new(),
+            data: temp_vec,
             binary: Vec::new(),
             Dictionary: dictionary::Instructions::new() 
         }
