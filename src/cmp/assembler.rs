84e86083c9bd8cb6f1b3579d1e6f7597674c38e5
arkasha401 <m1ramisama@gmail.com>
2022-08-12 14:46:20 +0300

    processing

--- src/cmp/assembler.rs
+++ src/cmp/assembler.rs
@@ -1,10 +1,9 @@
 use crate::cmp::dictionary;
+use core::panic;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-
 pub struct Assembler<'a> {
-    data: Vec<String>,
     binary: Vec<u8>,
     Dictionary: dictionary::Instructions<'a>,
 }
@@ -14,38 +13,40 @@ impl Assembler<'static> {
         let name = env::args().nth(1).unwrap();
         let f = File::open(name).unwrap();
         let reader = BufReader::new(f);
-        let mut temp_vec: Vec<String> = Vec::new();
+        let mut binary_vec: Vec<u8> = Vec::new();
         for (_index, line) in reader.lines().enumerate() {
             let _line = line.unwrap();
             let mut line = _line.trim().to_string();
             if !line.is_empty() {
                 if line.chars().last().unwrap() == ';' {
-                    println!("{}", line);
                     line.pop();
-                    println!("{}", line)
+                    //let temp_arr: Vec<&str> = line.split_whitespace().collect();
+                    //let operand = temp_arr[1].as_bytes();
+                    //println!("{:?}", operand);
+                    match temp_arr[0] {
+                        "LDM" => {
+                            if temp_arr[1].as_bytes()[0] <= 15 {
+                                binary_vec.push(0b11010000 + temp_arr[1].as_bytes()[0])
+                            } else {
+                                panic!()
+                            }
+                        }
+
+                        _ => panic!("ERROR: INVALID SYNTAX"),
+                    }
                 } else {
-                    println!("wtf")
+                    panic!("ERROR: ';' MIGHT BE MISSED")
                 }
             } else {
                 continue;
             }
         }
 
+        println!("{:?}", binary_vec);
+
         Assembler {
-            data: temp_vec,
-            binary: Vec::new(),
+            binary: binary_vec,
             Dictionary: dictionary::Instructions::new(),
         }
-    }
-
-    pub fn assembly(&mut self) {
-        let mut ptr: u16 = 0;
-        for ptr in 0..self.data.len() {}
-    }
-}
-
-impl Default for Assembler<'static> {
-    fn default() -> Self {
-        Self::new()
     }
 }
