15b5a97fd387f4217b3bf4395e054678b2a314d7
arkasha401 <m1ramisama@gmail.com>
2022-08-16 22:47:35 +0300

    a

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 mod memory;
 use memory::Memory;
-mod cpu;
 mod cmp;
-use std::env;
+mod cpu;
 use cpu::CPU;
-
-
+use std::env;
 
 pub fn main() {
-    let mems = memory::Memory::new(vec![0b0000,0b0000]);
+    let mems = memory::Memory::new(vec![0b0000, 0b0000]);
     let mut cpu = CPU::new(mems);
     let d = cmp::dictionary::Instructions::new();
     let mut c = cmp::assembler::Assembler::new();
     let filename: String = env::args().skip(1).collect();
     cpu.run();
-    
+
     c.compile(filename);
-    
 }
+
+nvim
